24c9d13de53e969781eda64b7b9bbb0c3fdc873a
sourav.kumar.all.2021@gmail.com <sourav.kumar.all.2021@gmail.com>
2023-05-08 17:11:00 +0530

    pwa 3

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
+import { proToJap, proToKanji } from './translate.js'
+
 navigator.serviceWorker.register('sw.js')
 
-// import { proToJap, proToKanji } from './translate.js'
+document.getElementById("add").addEventListener('click', () => {
+  window.location.assign('add.html')
+})
 
-// document.getElementById("add").addEventListener('click', () => {
-//   window.location.assign('add.html')
-// })
+const data = localStorage.getItem('jap') ? JSON.parse(localStorage.getItem('jap')) : []
+const dataEle = document.getElementById('data')
 
-// const data = localStorage.getItem('jap') ? JSON.parse(localStorage.getItem('jap')) : []
-// const dataEle = document.getElementById('data')
-
-// data.reverse().forEach((dat, i) => {
-//   const container = dataEle.appendChild(document.createElement('div'))
-//   const kanji = container.appendChild(document.createElement('h1'))
-//   const hiragana = container.appendChild(document.createElement('h2'))
-//   const subtitle = container.appendChild(document.createElement('span'))
-//   const meaning = container.appendChild(document.createElement('div'))
-//   const remove = container.appendChild(document.createElement('span'))
+data.reverse().forEach((dat, i) => {
+  const container = dataEle.appendChild(document.createElement('div'))
+  const kanji = container.appendChild(document.createElement('h1'))
+  const hiragana = container.appendChild(document.createElement('h2'))
+  const subtitle = container.appendChild(document.createElement('span'))
+  const meaning = container.appendChild(document.createElement('div'))
+  const remove = container.appendChild(document.createElement('span'))
   
-//   kanji.innerText = proToJap(proToKanji(dat.word))
-//   hiragana.innerText = proToJap(dat.word)
-//   subtitle.innerText = `\\${dat.word}\\`
-//   meaning.innerText = dat.meaning
+  kanji.innerText = proToJap(proToKanji(dat.word))
+  hiragana.innerText = proToJap(dat.word)
+  subtitle.innerText = `\\${dat.word}\\`
+  meaning.innerText = dat.meaning
   
-//   remove.classList.add('material-icons')
-//   remove.innerText = 'delete_forever'
+  remove.classList.add('material-icons')
+  remove.innerText = 'delete_forever'
   
-//   remove.onclick = () => {
-//     data.splice(i, 1)
-//     localStorage.setItem('jap', JSON.stringify(data))
-//     window.location.assign('index.html')
-//   }
-// })
+  remove.onclick = () => {
+    data.splice(i, 1)
+    localStorage.setItem('jap', JSON.stringify(data))
+    window.location.assign('index.html')
+  }
+})
