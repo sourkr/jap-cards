73e0b54bd92eddc7768a53c3227506a48b05491b
sourav.kumar.all.2021@gmail.com <sourav.kumar.all.2021@gmail.com>
2023-05-09 11:11:34 +0530

    delete old cache fixed & added dynamic cache

--- sw.js
+++ sw.js
@@ -1,17 +1,13 @@
-const KEY_STATIC = 'static_1.0'
+const KEY_STATIC = 'static_1.1'
 
 self.addEventListener('install', ev => {
   ev.waitUntil(
-    caches.open('static').then(cache => {
+    caches.open(KEY_STATIC).then(cache => {
       return cache.addAll([
         './',
         './main.js',
         './style/style.css',
         './style/sour.css',
-        './add.html',
-        './add.js',
-        './test.html',
-        './test.js',
         './words/hiragana.txt',
         './words/kanji.txt',
         './translate.js',
@@ -23,13 +19,13 @@ self.addEventListener('install', ev => {
   )
 })
 
-self.addEventListener('active', ev => {
+self.addEventListener('activate', ev => {
   ev.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
         keys
-        .filter(key => key !== KEY_STATIC)
-        .map(name => caches.delete(name))
+          .filter(key => key !== KEY_STATIC)
+          .map(name => caches.delete(name))
       )
     })
   )
@@ -38,7 +34,12 @@ self.addEventListener('active', ev => {
 self.addEventListener('fetch', ev => {
   ev.respondWith(
     caches.match(ev.request).then(response => {
-      return response || fetch(ev.request)
+      return response || fetch(ev.request).then(fRes => {
+        return caches.open('dynamic').then(cache => {
+          cache.put(ev.request, fRes)
+          return fRes
+        })
+      })
     })
   )
 })
