9b4ed742640ab33a0fcd5b2513b229465eb747b8
sourkr <sourav.kumar.all.2021@gmail.com>
2025-02-05 19:54:32 +0530

    revamped ui

--- add.js
+++ add.js
@@ -1,32 +1,39 @@
+import "https://code.jquery.com/jquery-3.7.1.slim.min.js"
 import { proToJap, proToKanji } from './translate.js'
 
-if (localStorage.getItem('theme') === 'dark') {
-  document.querySelector(':root').style.setProperty('--background', 'black')
-  document.querySelector(':root').style.setProperty('--color', 'white')
-}
+// if (localStorage.getItem('theme') === 'dark') {
+//   document.querySelector(':root').style.setProperty('--background', 'black')
+//   document.querySelector(':root').style.setProperty('--color', 'white')
+// }
+
+
+$('#btn').on('click', () => {
+    const word = $('#word').val().trim()
+    const meaning = $('#meaning').val().trim()
+
+    const data = localStorage.getItem('jap') ? JSON.parse(localStorage.getItem('jap')) : []
+    data.push({ word, meaning })
+    
+    console.log(data)
+    localStorage.setItem('jap', JSON.stringify(data))
+    location.reload()
+})
 
 document.getElementById('btn').addEventListener('click', () => {
-  const word = document.getElementById('word').value
-  const meaning = document.getElementById('meaning').value
 
-  const data = localStorage.getItem('jap') ? JSON.parse(localStorage.getItem('jap')) : []
-  data.push({word, meaning})
-  
-  localStorage.setItem('jap', JSON.stringify(data))
-  location.reload()
 })
 
-document.getElementById('word').addEventListener('input', function(){
-  document.querySelector('h1').innerText =
-  document.getElementById('kanji').innerText = proToJap(proToKanji(this.value))
-  
-  document.querySelector('h2').innerText = proToJap(this.value)
-})
+// document.getElementById('word').addEventListener('input', function() {
+//     document.querySelector('h1').innerText =
+//         document.getElementById('kanji').innerText = proToJap(proToKanji(this.value))
+
+//     document.querySelector('h2').innerText = proToJap(this.value)
+// })
 
 document.querySelectorAll('input')
-  .forEach(input => {
-    input.addEventListener('input', () => {
-      if(input.value) input.classList.add('valid')
-      else input.classList.remove(['valid'])
+    .forEach(input => {
+        input.addEventListener('input', () => {
+            if (input.value) input.classList.add('valid')
+            else input.classList.remove(['valid'])
+        })
     })
-  })
--- home/main.js
+++ home/main.js
@@ -1,40 +1,22 @@
+// import "https://code.jquery.com/jquery-3.7.1.slim.min.js"
 import { proToJap, proToKanji } from '../translate.js'
 
-if (localStorage.getItem('theme') === 'dark') {
-  document.querySelector(':root').style.setProperty('--background', 'black')
-  document.querySelector(':root').style.setProperty('--color', 'white')
-  document.querySelector(':root').style.setProperty('--nav', 'hsl(0, 0%, 10%)')
-}
-
-navigator.serviceWorker.register('sw.js')
+// navigator.serviceWorker.register('sw.js')
 
 const data = localStorage.getItem('jap') ? JSON.parse(localStorage.getItem('jap')) : []
 const dataEle = document.getElementById('data')
 const list = [...data].reverse()
 
-list.forEach((dat, i) => {
-  const container = dataEle.appendChild(document.createElement('div'))
-  const kanji = container.appendChild(document.createElement('h1'))
-  const hiragana = container.appendChild(document.createElement('h2'))
-  const subtitle = container.appendChild(document.createElement('span'))
-  const meaning = container.appendChild(document.createElement('div'))
-  const remove = container.appendChild(document.createElement('span'))
-  
-  kanji.innerText = proToJap(proToKanji(dat.word))
-  hiragana.innerText = proToJap(dat.word)
-  subtitle.innerText = `\\${dat.word}\\`
-  meaning.innerText = dat.meaning
-  
-  remove.classList.add('material-icons')
-  remove.innerText = 'delete_forever'
-  
-  remove.onclick = () => {
-    const index = data.indexOf(dat)
-    data.splice(index, 1)
+list.forEach((data, i) => {
+    const card = document.getElementById('card').content.cloneNode(true)
+    // console.log(card)
+    card.querySelector('.word').innerText = data.word
     
-    localStorage.setItem('jap', JSON.stringify(data))
-    location.reload()
-  }
+    card.querySelector('.edit').onclick = () => {
+        
+    }
+    
+    document.getElementById('data').appendChild(card)
 })
 
 document.getElementById("add").addEventListener('click', () => {
